b9161460374d652f738e8d98543f4e36cf190350
mdriyazakondo <mdriyazakondo265@gmail.com>
2025-11-18 18:49:18 +0600

    parcel api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -30,6 +30,63 @@ client
   .catch((err) => {
     console.log(err);
   });
+
+const db = client.db("zap_shit_db");
+const parcelCollection = db.collection("parcels");
+
+//======== parcel api ================//
+app.get("/parcels", async (req, res) => {
+  try {
+    const query = {};
+    const { email } = req.query;
+
+    if (email) {
+      query.senderEmail = email;
+    }
+
+    const result = await parcelCollection.find(query).toArray();
+    res
+      .status(200)
+      .send({ message: "parcel data successfully connect", result });
+  } catch (error) {
+    res.status(500).send({ message: "Intarnal server error" });
+  }
+});
+
+app.get("/parcels/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    const result = await parcelCollection.findOne(query);
+    res
+      .status(200)
+      .send({ message: "parcel single data successfully connect", result });
+  } catch (error) {
+    res.status(500).send({ message: "Intarnal server error" });
+  }
+});
+
+app.post("/parcels", async (req, res) => {
+  try {
+    const newParcels = req.body;
+    const create_at = new Date();
+    const result = await parcelCollection.insertOne(newParcels);
+    res.status(201).send({ message: "parcel post success", result });
+  } catch (error) {
+    res.status(500).send({ message: "Intarnal server error" });
+  }
+});
+
+app.delete("/parcels/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    const result = await parcelCollection.deleteOne(query);
+    res.status(200).send({ message: "parcel deleted successfully ", result });
+  } catch (error) {
+    res.status(500).send({ message: "Intarnal server error" });
+  }
+});
 
 app.get("/", (req, res) => {
   res.send("server is runnig port");
