844ce3e244c76b347ab48b0a83fc89376085bc46
mdriyazakondo <mdriyazakondo265@gmail.com>
2025-11-22 16:29:08 +0600

    payment api

--- index.js
+++ index.js
@@ -7,6 +7,16 @@ const stripe = require("stripe")(process.env.STRIPE_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
+const crypto = require("crypto");
+
+function generateTrackingId() {
+  const prefix = "PRCL"; // your brand prefix
+  const date = new Date().toISOString().slice(0, 10).replace(/-/g, "");
+  const random = crypto.randomBytes(3).toString("hex").toUpperCase();
+
+  return `${prefix}-${date}-${random}`;
+}
+
 // middleware
 app.use(cors());
 app.use(express.json());
@@ -35,6 +45,7 @@ client
 
 const db = client.db("zap_shit_db");
 const parcelCollection = db.collection("parcels");
+const paymentCollection = db.collection("payments");
 
 //======== parcel api ================//
 app.get("/parcels", async (req, res) => {
@@ -91,10 +102,42 @@ app.delete("/parcels/:id", async (req, res) => {
 });
 
 //===== payment releted api ============//
+// new api
+
+app.post("/payment-checkout-session", async (req, res) => {
+  const paymentInfo = req.body;
+  const amout = parseInt(paymentInfo.cost) * 100;
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        price_data: {
+          currency: "USD",
+          unit_amount: amout,
+          product_data: {
+            name: `Please pay for ${paymentInfo.parcelName}`,
+          },
+        },
+        quantity: 1,
+      },
+    ],
+    customer_email: paymentInfo.senderEmail,
+    metadata: {
+      parcelId: paymentInfo.parcelId,
+      parcelName: paymentInfo.parcelName,
+    },
+    mode: "payment",
+    success_url: `${process.env.SITE_DOMIN}/dashboard/payment-success?session_id={CHECKOUT_SESSION_ID}`,
+    cancel_url: `${process.env.SITE_DOMIN}/dashboard/payment-cancelled`,
+  });
+  // console.log(session);
+  res.send({ url: session.url });
+});
+
+// old api
 app.post("/stripe-checkout-sessions", async (req, res) => {
   const paymentInfo = req.body;
   const amount = parseInt(paymentInfo.cost) * 100;
-  const session = await stripe.checkout.sessions.create({
+  const session = stripe.checkout.sessions.create({
     line_items: [
       {
         price_data: {
@@ -117,6 +160,67 @@ app.post("/stripe-checkout-sessions", async (req, res) => {
   });
   console.log(session);
   res.send({ url: session.url });
+});
+
+app.patch("/payment-success", async (req, res) => {
+  const sessionId = req.query.session_id;
+  const session = await stripe.checkout.sessions.retrieve(sessionId);
+  const transictionId = session.payment_intent;
+
+  const exitPayment = await paymentCollection.findOne({ transictionId });
+  if (exitPayment) {
+    return res.send({
+      message: "already exiting payment",
+      transictionId,
+      trackingId: exitPayment.trackingId,
+    });
+  }
+
+  const trackingId = generateTrackingId();
+  if (session.payment_status === "paid") {
+    const id = session.metadata.parcelId;
+    const query = { _id: new ObjectId(id) };
+    const update = {
+      $set: {
+        payment_status: "paid",
+        trackingId: trackingId,
+      },
+    };
+    const result = await parcelCollection.updateOne(query, update);
+    const payment = {
+      amount: session.amount_total,
+      currency: session.currency,
+      customerEmail: session.customer_email,
+      parcelId: session.metadata.parcelId,
+      parcelName: session.metadata.parcelName,
+      transictionId: session.payment_intent,
+      paymentStatus: session.payment_status,
+      paymentA: new Date(),
+      trackingId: trackingId,
+    };
+    if (session.payment_status === "paid") {
+      const resultPayment = await paymentCollection.insertOne(payment);
+      return res.send({
+        success: true,
+        modifyParcel: result,
+        trackingId: trackingId,
+        transictionId: session.payment_intent,
+        paymentInfo: resultPayment,
+      });
+    }
+  }
+  return res.send({ success: false });
+});
+
+// payment releted history
+app.get("/payments", async (req, res) => {
+  const email = req.query.email;
+  const query = {};
+  if (email) {
+    query.customerEmail = email;
+  }
+  const result = await paymentCollection.find(query).toArray();
+  res.send({ message: "payments successfully", result });
 });
 
 app.get("/", (req, res) => {
